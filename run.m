06774115b609418c2d4fac808b3e265bbf2cf363
dankusanovic <dankusanovic@gmail.com>
2023-06-12 10:23:41 -0400

    Provides the load of the files

--- run.m
+++ run.m
@@ -13,10 +13,12 @@ fprintf('Universidad Tecnica Federico Santa Maria\n');
 fprintf('Departamento de Obras Civiles\n');
 
 %TODO: Specify path to load file
-modelpath ='/path/to/file.txt';
+modelpath ='/home/danilo/Documents/01-Lectures/AE_CIV234/Homework/AE_2D/Examples/Marco2D.txt';
 
 %TODO: Parse the file located at modelpath
+MESH = ParseSimulationData(modelpath);
 
 %TODO: Perform the static analysis
+plotModel(MESH)
 
 %TODO: Display the deformed shape
